4ac012845ff522501e2f773a4f4092c2c8fe2b61
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-05-14 01:11:23 +0530

    sign up page completed

--- src/hoc/Router.jsx
+++ src/hoc/Router.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Landing from "../pages/LandingPage/Landing";
+import SignUp from "../pages/SignUpPage/SignUp";
 
 const Router = () => {
   return (
     <>
       <Routes>
         <Route exact path="/" element={<Landing />}></Route>
+        <Route exact path="/sign-up" element={<SignUp />}></Route>
       </Routes>
     </>
   );
---
+++ src/pages/SignUpPage/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Hero } from "../../containers/SignUpPage";
+
+const SignUp = () => {
+  
+  return (
+    <>
+      <Hero/>
+    </>
+  );
+};
+
+export default SignUp;
